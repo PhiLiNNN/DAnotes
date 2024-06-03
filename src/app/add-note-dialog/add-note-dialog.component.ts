c1c262bc504c88db2c7ce4376775f8217b5eb6ad
PhiLiNNN <philipp_wendschuch@web.de>
2024-06-03 13:42:03 +0200

    first commit

---
+++ src/app/add-note-dialog/add-note-dialog.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Note } from '../interfaces/note.interface';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { NoteListService } from '../firebase-services/note-list.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-note-dialog',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './add-note-dialog.component.html',
+  styleUrl: './add-note-dialog.component.scss',
+})
+export class AddNoteDialogComponent {
+  @Output() addDialogClosed: EventEmitter<boolean> = new EventEmitter();
+  title = '';
+  description = '';
+
+  constructor(public noteService: NoteListService) {}
+  closeDialog() {
+    this.title = '';
+    this.description = '';
+    this.addDialogClosed.emit(false);
+  }
+
+  addNote() {
+    //beachte das closeDialog() zum Schluss kommt, denn es leert die Variablen
+    this.closeDialog();
+  }
+}
---
+++ src/app/firebase-services/note-list.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  doc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+  items$: Observable<Note[]>;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.items$ = collectionData(this.getNotesRef());
+  }
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
+}
